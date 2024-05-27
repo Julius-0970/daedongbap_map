08bda7cfc3db18ee07a4a77c6f418fff43d31775
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-05-28 02:00:45 +0900

    찐최종 데이터베이스

--- daedongbap_map_dump.sql
+++ daedongbap_map_dump.sql
@@ -16,34 +16,6 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `address`
---
-
-DROP TABLE IF EXISTS `address`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `address` (
-  `address_number` varchar(255) NOT NULL,
-  `Restaurant_Address` varchar(255) NOT NULL,
-  `resident_id` varchar(255) NOT NULL,
-  `address_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`address_number`),
-  KEY `resident_id` (`resident_id`),
-  CONSTRAINT `address_ibfk_1` FOREIGN KEY (`resident_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `address`
---
-
-LOCK TABLES `address` WRITE;
-/*!40000 ALTER TABLE `address` DISABLE KEYS */;
-INSERT INTO `address` VALUES ('2394ulwsk','충청남도 아산시 탕정면 선문로221번길 70','yhr1435','대학교');
-/*!40000 ALTER TABLE `address` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `alarm`
 --
 
@@ -55,7 +27,7 @@ CREATE TABLE `alarm` (
   `Receiver_ID` varchar(255) NOT NULL,
   `Message` varchar(10000) NOT NULL,
   `Confirmation_Status` tinyint(1) NOT NULL,
-  `Transmission_Time` datetime NOT NULL,
+  `Transmission_Time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Alarm_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -107,7 +79,7 @@ CREATE TABLE `comment` (
   `comment_writer_ID` varchar(255) NOT NULL,
   `comment_Text` varchar(5000) NOT NULL,
   `comment_like_number` int NOT NULL,
-  `comment_write_date` datetime NOT NULL,
+  `comment_write_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Comment_ID`),
   KEY `Parent_Comment_ID` (`Parent_Comment_ID`),
   KEY `feed_id` (`feed_id`),
@@ -124,8 +96,36 @@ CREATE TABLE `comment` (
 
 LOCK TABLES `comment` WRITE;
 /*!40000 ALTER TABLE `comment` DISABLE KEYS */;
-INSERT INTO `comment` VALUES ('asd234',NULL,'sdf80s9','yhr1435','이것은 댓글이다',3,'2024-03-10 00:05:49');
+INSERT INTO `comment` VALUES ('asd234',NULL,'sdf80s9','yhr1435','이것은 댓글이다',0,'2024-05-17 15:34:05');
 /*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comment_like`
+--
+
+DROP TABLE IF EXISTS `comment_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment_like` (
+  `Like_ID` int NOT NULL AUTO_INCREMENT,
+  `Liker_ID` varchar(255) NOT NULL,
+  `Comment_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`Like_ID`),
+  KEY `Liker_ID` (`Liker_ID`),
+  KEY `Comment_ID` (`Comment_ID`),
+  CONSTRAINT `comment_like_ibfk_1` FOREIGN KEY (`Liker_ID`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `comment_like_ibfk_2` FOREIGN KEY (`Comment_ID`) REFERENCES `comment` (`Comment_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comment_like`
+--
+
+LOCK TABLES `comment_like` WRITE;
+/*!40000 ALTER TABLE `comment_like` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment_like` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -141,7 +141,7 @@ CREATE TABLE `feed` (
   `title` varchar(255) NOT NULL,
   `feed_content` varchar(10000) NOT NULL,
   `feed_like_number` int NOT NULL,
-  `feed_write_date` datetime NOT NULL,
+  `feed_write_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`feed_ID`),
   KEY `feed_writer_id` (`feed_writer_id`),
   CONSTRAINT `feed_ibfk_1` FOREIGN KEY (`feed_writer_id`) REFERENCES `user` (`user_id`)
@@ -154,65 +154,36 @@ CREATE TABLE `feed` (
 
 LOCK TABLES `feed` WRITE;
 /*!40000 ALTER TABLE `feed` DISABLE KEYS */;
-INSERT INTO `feed` VALUES ('sdf80s9','yhr1435','소개','이 웹은 한국에서 ~asdasdasdasdasdasdasdas',10,'2024-03-11 00:00:00');
+INSERT INTO `feed` VALUES ('sdf80s9','yhr1435','소개','이 웹은 한국에서 ~asdasdasdasdasdasdasdas',10,'2024-05-17 15:30:12');
 /*!40000 ALTER TABLE `feed` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `item_type`
+-- Table structure for table `feed_like`
 --
 
-DROP TABLE IF EXISTS `item_type`;
+DROP TABLE IF EXISTS `feed_like`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `item_type` (
-  `Type_ID` int NOT NULL AUTO_INCREMENT,
-  `Type_Name` varchar(40) NOT NULL,
-  PRIMARY KEY (`Type_ID`),
-  UNIQUE KEY `Type_Name` (`Type_Name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `item_type`
---
-
-LOCK TABLES `item_type` WRITE;
-/*!40000 ALTER TABLE `item_type` DISABLE KEYS */;
-INSERT INTO `item_type` VALUES (2,'댓글'),(3,'리뷰'),(4,'식당'),(1,'피드');
-/*!40000 ALTER TABLE `item_type` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `like_`
---
-
-DROP TABLE IF EXISTS `like_`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `like_` (
-  `Like_Number` int NOT NULL,
+CREATE TABLE `feed_like` (
+  `Like_ID` int NOT NULL AUTO_INCREMENT,
   `Liker_ID` varchar(255) NOT NULL,
-  `Liked_Item_User` varchar(255) NOT NULL,
-  `Liked_Item_ID` varchar(255) NOT NULL,
-  `Liked_Item_Type_ID` int NOT NULL,
-  PRIMARY KEY (`Like_Number`),
+  `Feed_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`Like_ID`),
   KEY `Liker_ID` (`Liker_ID`),
-  KEY `Liked_Item_User` (`Liked_Item_User`),
-  KEY `Liked_Item_Type_ID` (`Liked_Item_Type_ID`),
-  CONSTRAINT `like__ibfk_1` FOREIGN KEY (`Liker_ID`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `like__ibfk_2` FOREIGN KEY (`Liked_Item_User`) REFERENCES `user` (`user_id`),
-  CONSTRAINT `like__ibfk_3` FOREIGN KEY (`Liked_Item_Type_ID`) REFERENCES `item_type` (`Type_ID`)
+  KEY `Feed_ID` (`Feed_ID`),
+  CONSTRAINT `feed_like_ibfk_1` FOREIGN KEY (`Liker_ID`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `feed_like_ibfk_2` FOREIGN KEY (`Feed_ID`) REFERENCES `feed` (`feed_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `like_`
+-- Dumping data for table `feed_like`
 --
 
-LOCK TABLES `like_` WRITE;
-/*!40000 ALTER TABLE `like_` DISABLE KEYS */;
-/*!40000 ALTER TABLE `like_` ENABLE KEYS */;
+LOCK TABLES `feed_like` WRITE;
+/*!40000 ALTER TABLE `feed_like` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feed_like` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -228,7 +199,7 @@ CREATE TABLE `restaurant` (
   `Restaurant_Address` varchar(255) NOT NULL,
   `restaurant_like_number` int NOT NULL,
   `Category_Number` int DEFAULT NULL,
-  `restaurant_upload_date` datetime NOT NULL,
+  `restaurant_upload_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`Restaurant_ID`),
   KEY `Category_Number` (`Category_Number`),
   CONSTRAINT `restaurant_ibfk_1` FOREIGN KEY (`Category_Number`) REFERENCES `category` (`Category_Number`)
@@ -241,7 +212,36 @@ CREATE TABLE `restaurant` (
 
 LOCK TABLES `restaurant` WRITE;
 /*!40000 ALTER TABLE `restaurant` DISABLE KEYS */;
+INSERT INTO `restaurant` VALUES ('RES123','밥집','서울특별시 어쩌구 저쩌구',1004,1,'2024-05-17 15:36:09');
 /*!40000 ALTER TABLE `restaurant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `restaurant_like`
+--
+
+DROP TABLE IF EXISTS `restaurant_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `restaurant_like` (
+  `Like_ID` int NOT NULL AUTO_INCREMENT,
+  `Liker_ID` varchar(255) NOT NULL,
+  `Restaurant_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`Like_ID`),
+  KEY `Liker_ID` (`Liker_ID`),
+  KEY `Restaurant_ID` (`Restaurant_ID`),
+  CONSTRAINT `restaurant_like_ibfk_1` FOREIGN KEY (`Liker_ID`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `restaurant_like_ibfk_2` FOREIGN KEY (`Restaurant_ID`) REFERENCES `restaurant` (`Restaurant_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `restaurant_like`
+--
+
+LOCK TABLES `restaurant_like` WRITE;
+/*!40000 ALTER TABLE `restaurant_like` DISABLE KEYS */;
+/*!40000 ALTER TABLE `restaurant_like` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -257,8 +257,8 @@ CREATE TABLE `review` (
   `review_writer_ID` varchar(255) NOT NULL,
   `review_content` varchar(10000) NOT NULL,
   `review_like_number` int NOT NULL,
-  `rating` tinyint NOT NULL,
-  `review_write_date` datetime NOT NULL,
+  `rating` decimal(2,1) NOT NULL,
+  `review_write_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`review_ID`),
   KEY `review_writer_ID` (`review_writer_ID`),
   KEY `restaurant_id` (`restaurant_id`),
@@ -273,7 +273,36 @@ CREATE TABLE `review` (
 
 LOCK TABLES `review` WRITE;
 /*!40000 ALTER TABLE `review` DISABLE KEYS */;
+INSERT INTO `review` VALUES ('dr0dfgj','RES123','yhr1435','맛집이나 평범하다',11,4.5,'2024-05-18 09:17:58');
 /*!40000 ALTER TABLE `review` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `review_like`
+--
+
+DROP TABLE IF EXISTS `review_like`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `review_like` (
+  `Like_ID` int NOT NULL AUTO_INCREMENT,
+  `Liker_ID` varchar(255) NOT NULL,
+  `Review_ID` varchar(255) NOT NULL,
+  PRIMARY KEY (`Like_ID`),
+  KEY `Liker_ID` (`Liker_ID`),
+  KEY `Review_ID` (`Review_ID`),
+  CONSTRAINT `review_like_ibfk_1` FOREIGN KEY (`Liker_ID`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `review_like_ibfk_2` FOREIGN KEY (`Review_ID`) REFERENCES `review` (`review_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `review_like`
+--
+
+LOCK TABLES `review_like` WRITE;
+/*!40000 ALTER TABLE `review_like` DISABLE KEYS */;
+/*!40000 ALTER TABLE `review_like` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -290,7 +319,7 @@ CREATE TABLE `user` (
   `age` int NOT NULL,
   `gender` tinyint NOT NULL,
   `email` varchar(255) NOT NULL,
-  `user_create_date` datetime NOT NULL,
+  `user_create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -301,7 +330,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES ('yhr1435','sdfsdfs','정희원',24,0,'yhr1435@gmail.com','2024-03-15 00:00:00');
+INSERT INTO `user` VALUES ('sjmdfs1','1111','익명',30,0,'sjdhnfs@sdf.com','2024-05-18 11:30:08'),('yhr1435','sdfsdfs','정희원',24,0,'yhr1435@gmail.com','2024-05-18 09:13:35');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -314,4 +343,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-17 17:53:53
+-- Dump completed on 2024-05-28  1:59:28
