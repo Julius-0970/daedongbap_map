52c650c791e3ac805535c45842c12afd571693e1
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-06-01 00:24:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # 라우트를 user_routes에서 가져오기
 import user_routes
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
