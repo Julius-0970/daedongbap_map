0d0cbe3597d55554a31bd1c9b3540edc33c14a2a
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-06-03 15:58:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 
-# 라우트를 user_routes에서 가져오기
+# MySQL 데이터베이스 URI 설정
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI', 'mysql+pymysql://username:password@localhost/db_name')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# SQLAlchemy 객체 생성
+db = SQLAlchemy(app)
+
+# user_routes와 search_routes를 가져오기
 import user_routes
 import search_routes
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
