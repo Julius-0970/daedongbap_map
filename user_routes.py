63384a71a59be35ac4a82d927bbb800fafecd50a
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-06-12 20:48:25 +0900

    Update user_routes.py

--- user_routes.py
+++ user_routes.py
@@ -1,20 +1,17 @@
-from flask import Blueprint, Flask, request, jsonify, render_template, session
+from flask import Flask, request, jsonify, render_template, session
 import pymysql
 import hashlib
 import sys
-from dotenv import load_dotenv
 import os  # os 모듈 임포트
-
-# .env 파일의 환경 변수를 읽어들입니다.
-load_dotenv()
 
 user_routes = Blueprint('user_routes', __name__)
 
 db_config = {
     'host': os.getenv('DATABASE_HOST'),
+    'port': int(os.getenv('DATABASE_PORT')),
     'user': os.getenv('DATABASE_USER'),
     'password': os.getenv('DATABASE_PASSWORD'),
-    'database': os.getenv('DATABASE_NAME'),
+    'database': os.getenv('DATABASE_DB'),
     'charset': 'utf8mb4',
     'cursorclass': pymysql.cursors.DictCursor
 }
@@ -23,6 +20,7 @@ db_config = {
 def connect_db():
     return pymysql.connect(
         host=db_config['host'],
+        port=db_config['port'],
         user=db_config['user'],
         password=db_config['password'],
         database=db_config['database'],
