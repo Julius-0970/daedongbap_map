e21843f4702b8068051658b0aa07d79c5b2dd100
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-06-13 13:55:26 +0900

    Update search_routes.py

--- search_routes.py
+++ search_routes.py
@@ -1,15 +1,12 @@
 from flask import Blueprint, Flask, request, jsonify, render_template, session
 import pymysql
-from dotenv import load_dotenv
 import os  # os 모듈 임포트
-
-# .env 파일의 환경 변수를 읽어들입니다.
-load_dotenv()
 
 search_routes = Blueprint('search_routes', __name__)
 
 db_config = {
     'host': os.getenv('DATABASE_HOST'),
+    'port': int(os.getenv('DATABASE_PORT')),
     'user': os.getenv('DATABASE_USER'),
     'password': os.getenv('DATABASE_PASSWORD'),
     'database': os.getenv('DATABASE_NAME'),
@@ -21,12 +18,15 @@ db_config = {
 def connect_db():
     return pymysql.connect(
         host=db_config['host'],
+        port=db_config['port'],
         user=db_config['user'],
         password=db_config['password'],
         database=db_config['database'],
         charset=db_config['charset'],
         cursorclass=db_config['cursorclass']
     )
+
+#검색 기능 구현_모든 값을 찾아서 전달
 @search_routes.route('/search', methods=['POST'])
 def search():
     data = request.json
