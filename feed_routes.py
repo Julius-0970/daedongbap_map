d5aad4c68bb83ab2d0d16b6d7080b3340e91386c
Julius-0970 <162962316+Julius-0970@users.noreply.github.com>
2024-06-13 13:42:32 +0900

    Update feed_routes.py

--- feed_routes.py
+++ feed_routes.py
@@ -1,17 +1,14 @@
-from flask import Blueprint, request, jsonify, session
+from flask import Blueprint, Flask, request, jsonify, session
 import pymysql
 import uuid
-from dotenv import load_dotenv
 import os  # os 모듈 임포트
-
-# .env 파일의 환경 변수를 읽어들입니다.
-load_dotenv()
 
 feed_routes = Blueprint('feed_routes', __name__)
 
 
 db_config = {
     'host': os.getenv('DATABASE_HOST'),
+    'port': int(os.getenv('DATABASE_PORT')),
     'user': os.getenv('DATABASE_USER'),
     'password': os.getenv('DATABASE_PASSWORD'),
     'database': os.getenv('DATABASE_NAME'),
@@ -23,6 +20,7 @@ db_config = {
 def connect_db():
     return pymysql.connect(
         host=db_config['host'],
+         port=db_config['port'],
         user=db_config['user'],
         password=db_config['password'],
         database=db_config['database'],
